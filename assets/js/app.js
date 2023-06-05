ce1d4cf56c960551de4117fa31f8a23ef103c344
heispapijay <heispapijay@gmail.com>
2023-06-05 21:52:25 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,32 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu'),
+        navToggler = document.getElementById('nav-toggler'),
+        navClose = document.getElementById('nav-closer')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggler){
+    navToggler.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.getElementById('.nav_link')
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu') //click on nav-link to remove show
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
